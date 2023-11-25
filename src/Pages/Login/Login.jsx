78de633c1a153a7ae9b61c2397981142880aa9d1
Md Nirab <nabilnirab255@gmail.com>
2023-11-25 19:41:01 +0600

    install firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -28,6 +28,22 @@ const Login = () => {
                                     up
                                 </Link>.
                             </p>
+                            <div className="text-center form-control mt-4">
+                                <button onClick={'F'} className="text-black font-bold border-black border-2 py-2 rounded-md flex justify-center items-center px-5"> <span className="mr-2">Google</span>
+                                    <svg viewBox="0 0 48 48" className="w-5 mr-2">
+                                        <title>Google Logo</title>
+                                        <clipPath id="g">
+                                            <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" />
+                                        </clipPath>
+                                        <g className="colors" clipPath="url(#g)">
+                                            <path fill="#FBBC05" d="M0 37V11l17 13z" />
+                                            <path fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
+                                            <path fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
+                                            <path fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
+                                        </g>
+                                    </svg>
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
